a4e2adb86d1a903290719ec5139047bc6729268a
NAFISA ALAM <37190335+NafuKrypto@users.noreply.github.com>
2021-07-09 18:53:09 +0600

    Alert Dialog 4th push

--- app/src/main/java/com/example/alertdialogue/MainActivity.java
+++ app/src/main/java/com/example/alertdialogue/MainActivity.java
@@ -5,20 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
- String language;
 
+    String name_str,age_str,address_str,id_str;
+    TextView textViewName,textViewAge,textViewID,textViewAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+         textViewName=findViewById( R.id.name_person );
+         textViewAge=findViewById( R.id.age_person );
+         textViewID=findViewById( R.id.id_person );
+         textViewAdd=findViewById( R.id.add_person );
 
         Button button =findViewById( R.id.btn );
 
@@ -26,34 +36,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                AlertDialog.Builder alertdialog =new AlertDialog.Builder( MainActivity.this );
-                alertdialog.setTitle( "Choose any one language" );
-                String[] items={"Bengal","English","Korean"};
-                int checkeditem=0;
-                alertdialog.setSingleChoiceItems( items, checkeditem, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+               /* AlertDialog.Builder alertdialog =new AlertDialog.Builder( MainActivity.this );
+                alertdialog.setTitle( "Fill up the blanks" );
+                final EditText name = new EditText(MainActivity.this);
+                final EditText age = new EditText(MainActivity.this);
+                final EditText id = new EditText(MainActivity.this);
+                final EditText address = new EditText(MainActivity.this);
 
-                        switch (i){
-                            case 0:
-                                language="Bengal";
-                                break;
-                            case 1:
-                                language="English";
-                                break;
-                            case 2:
-                                language="Korean";
-                                break;
-                        }
-                    }
-                } );
+                name.setHint( "Name" );  //editbox1 hint
+                name.setGravity( Gravity.CENTER ); //editbox in center
+                name.setBackgroundResource( R.drawable.editbox); //editbox style design
 
+                age.setHint( "Age" );  //editbox2 hint
+                age.setGravity( Gravity.CENTER );
+                age.setBackgroundResource( R.drawable.editbox);
+
+                id.setHint( "ID" ); //editbox3 hint
+                id.setGravity( Gravity.CENTER );
+                id.setBackgroundResource( R.drawable.editbox);
+
+                address.setHint( "Address" );//editbox4 hint
+                address.setGravity( Gravity.CENTER );
+                address.setBackgroundResource( R.drawable.editbox);
+
+                //set up in a linear layout
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                layoutParams.setMargins( 20,20,20,20);
+
+                LinearLayout lp=new LinearLayout( getApplicationContext() );
+                lp.setOrientation( LinearLayout.VERTICAL );
+
+                lp.addView( name ,layoutParams);
+                lp.addView( age,layoutParams );
+                lp.addView( id ,layoutParams);
+                lp.addView( address ,layoutParams);
+                alertdialog.setView( lp );
                 alertdialog.setPositiveButton( "OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-
                         dialogInterface.dismiss();
-                        Toast.makeText(MainActivity.this, "You have chosen "+language,Toast.LENGTH_LONG ).show();
+                         name_str = name.getText().toString().trim();
+                         age_str=age.getText().toString().trim();
+                         id_str=id.getText().toString().trim();
+                         address_str=address.getText().toString().trim();
+                        //Display everything
+                        textViewName.setText( "Name: "+name_str );
+                        textViewAge.setText( "Age: "+age_str );
+                        textViewID.setText( "ID: "+id_str );
+                        textViewAdd.setText( "Address: "+address_str );
+
                     }
                 } );
 
@@ -66,9 +98,30 @@ public class MainActivity extends AppCompatActivity {
 
                 AlertDialog alert=alertdialog.create();
                 alert.setCanceledOnTouchOutside( false );
-                alert.show();
+                alert.show();*/
+               showAlertDialog();
             }
         } );
 
+
+
+
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        final View customLayout = getLayoutInflater().inflate(R.layout.custom, null);
+        alertDialog.setView(customLayout);
+        alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // send data from the AlertDialog to the Activity
+                //EditText editText = customLayout.findViewById(R.id.editText);
+                //Toast.makeText(MainActivity.this,editText.getText().toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog alert = alertDialog.create();
+        alert.setCanceledOnTouchOutside(false);
+        alert.show();
     }
 }
